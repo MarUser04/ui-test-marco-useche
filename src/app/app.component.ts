1f03a059abdacc1af1c153ba721800d4801dd55e
Marco Useche <usechermarcoj@gmail.com>
2021-12-25 15:02:18 -0400

    section tilte and display style button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ui-test-marco-useche';
+  showOptions = false;
+  displayFormat = 'grid';
+  allowChangeFormat = true;
+
+  ngOnInit(): void {
+    if (window.innerWidth <= 550) {
+      this.allowChangeFormat = false;
+    } else {
+      this.allowChangeFormat = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth <= 550) {
+      this.allowChangeFormat = false;
+      this.displayFormat = 'grid';
+    } else {
+      this.allowChangeFormat = true;
+    }
+  }
+
+  displayOptions() {
+    this.showOptions = !this.showOptions;
+  }
+
+  changeFormat(format: string) {
+    this.displayFormat = format;
+  }
 }
